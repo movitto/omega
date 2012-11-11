b4c9f8fdad4eb1a10a0570513125053593ac3f6e
Mo Morsi <mo@morsi.org>
2012-11-11 07:17:32 -0500

    tidy up js client files, create omega namespace

--- site2/source/javascripts/handlers.js
+++
@@ -1,9 +0,0 @@
-/* Omega Default Handlers
- *
- * Copyright (C) 2012 Mohammed Morsi <mo@morsi.org>
- *  Licensed under the AGPLv3+ http://www.gnu.org/licenses/agpl.txt
- */
-
-/////////////////////////////////////// public methods
-
-/////////////////////////////////////// private methods
