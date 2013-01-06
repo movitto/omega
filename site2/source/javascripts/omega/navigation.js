3185dc7049cfa880e67d2871db6bcdef9d227eaa
Mo Morsi <mo@morsi.org>
2013-01-06 10:14:14 -0500

    encapsulate navigation container

--- site2/source/javascripts/omega/navigation.js
+++ site2/source/javascripts/omega/navigation.js
@@ -4,89 +4,115 @@
  *  Licensed under the AGPLv3+ http://www.gnu.org/licenses/agpl.txt
  */
 
-/////////////////////////////////////// private methods
+/////////////////////////////////////// Omega Navigation Container
 
-/* Show login controls, hide logout controls
+/* Initialize new Omega Navigation Container
  */
-function show_login_controls(){
-  $('#register_link').show();
-  $('#login_link').show();
-  $('#logout_link').hide();
-  $('#account_link').hide();
-}
+function OmegaNavigationContainer(){
 
-/* Show logout controls, hide login controls
- */
-function show_logout_controls(){
-  $('#account_link').show();
-  $('#logout_link').show();
-  $('#login_link').hide();
-  $('#register_link').hide();
-}
+  /////////////////////////////////////// private data
 
-/* Show the login dialog
- */
-function show_login_dialog(){
-  $omega_dialog.show('Login', '#login_dialog');
-}
+  var register_link       = $('#register_link');
 
-/* Submit the login dialog
- */
-function submit_login_dialog(){
-  var user_id = $('#omega_dialog #login_username').attr('value');
-  var user_password = $('#omega_dialog #login_password').attr('value');
-  var user = new OmegaUser({ id : user_id, password : user_password });
-  $omega_dialog.hide();
-  $omega_session.login_user(user);
-}
+  var login_link          = $('#login_link');
 
-/* Log the user out
- */
-function handle_logout_click(){
-  $omega_session.logout_user();
-  show_login_controls();
-}
+  var logout_link         = $('#logout_link');
 
-/* Show registration dialog
- */
-function show_register_dialog(){
-  $omega_dialog.show('Create Account', '#register_dialog');
+  var account_link        = $('#account_link');
 
-  // FIXME make recaptcha public key variable / configurable
-  $('#omega_dialog #omega_recaptcha').html('<div id="omega_registration_recaptcha"></div>');
-  Recaptcha.create("6LflM9QSAAAAAHsPkhWc7OPrwV4_AYZfnhWh3e3n", "omega_registration_recaptcha",
-                   { theme: "red", callback: Recaptcha.focus_response_field});
-}
+  var login_usernamme     = $('#omega_dialog #login_username');
 
-/* Handle registration submitted response
- */
-function callback_registration_submitted(user, error){
-  if(error){
-    $omega_dialog.show('Failed to create account', '#registration_failed_dialog', error['message'])
-  }else{
-    $omega_dialog.show('Creating Account', '#registration_submitted_dialog')
+  var login_password      = $('#omega_dialog #login_password');
+
+  var recaptcha_container = $('#omega_dialog #omega_recaptcha');
+
+  var register_username   = $('#omega_dialog #register_username');
+  
+  var register_password   = $('#omega_dialog #register_password');
+
+  var register_email      = $('#omega_dialog #register_email');
+
+
+  /////////////////////////////////////// private methods
+
+  /* Show login controls, hide logout controls
+   */
+  var show_login_controls = function(){
+    register_link.show(); login_link.show();
+    account_link.hide();  logout_link.hide();
+    
   }
-}
 
-/* Submit register user dialog
- */
-function submit_register_dialog(){
-  var user_id = $('#omega_dialog #register_username').attr('value');
-  var user_password = $('#omega_dialog #register_password').attr('value');
-  var user_email    = $('#omega_dialog #register_email').attr('value');
-  var recaptcha_challenge = Recaptcha.get_challenge();
-  var recaptcha_response  = Recaptcha.get_response();
-  var user = new OmegaUser({ id : user_id, password : user_password, email : user_email,
-                        recaptcha_challenge : recaptcha_challenge,
-                        recaptcha_response : recaptcha_response});
+  /* Show logout controls, hide login controls
+   */
+  var show_logout_controls = function(){
+    account_link.show();  logout_link.show();
+    register_link.hide(); login_link.hide();
+  }
 
-  $omega_dialog.hide();
-  $omega_session.register_user(user, callback_registration_submitted);
-}
+  /* Show the login dialog
+   */
+  var show_login_dialog = function(){
+    $omega_dialog.show('Login', '#login_dialog');
+  }
 
-/////////////////////////////////////// initialization
+  /* Submit the login dialog
+   */
+  var submit_login_dialog = function(){
+    var user_id       = login_username.attr('value');
+    var user_password = login_password.attr('value');
+    var user = new OmegaUser({ id : user_id, password : user_password });
+    $omega_dialog.hide();
+    $omega_session.login_user(user);
+  }
 
-$(document).ready(function(){ 
+  /* Log the user out
+   */
+  var handle_logout_click = function(){
+    $omega_session.logout_user();
+    show_login_controls();
+  }
+
+  /* Show registration dialog
+   */
+  var show_register_dialog = function(){
+    $omega_dialog.show('Create Account', '#register_dialog');
+  
+    // FIXME make recaptcha public key variable / configurable
+    recaptcha_container.html('<div id="omega_registration_recaptcha"></div>');
+    Recaptcha.create("6LflM9QSAAAAAHsPkhWc7OPrwV4_AYZfnhWh3e3n", "omega_registration_recaptcha",
+                     { theme: "red", callback: Recaptcha.focus_response_field});
+  }
+
+  /* Handle registration submitted response
+   */
+  var callback_registration_submitted = function(user, error){
+    if(error){
+      $omega_dialog.show('Failed to create account', '#registration_failed_dialog', error['message'])
+    }else{
+      $omega_dialog.show('Creating Account', '#registration_submitted_dialog')
+    }
+  }
+
+  /* Submit register user dialog
+   */
+  var submit_register_dialog = function(){
+    var user_id             = register_username.attr('value');
+    var user_password       = register_password.attr('value');
+    var user_email          = register_email.attr('value');
+    var recaptcha_challenge = Recaptcha.get_challenge();
+    var recaptcha_response  = Recaptcha.get_response();
+
+    var user = new OmegaUser({ id : user_id, password : user_password, email : user_email,
+                          recaptcha_challenge : recaptcha_challenge,
+                          recaptcha_response : recaptcha_response});
+  
+    $omega_dialog.hide();
+    $omega_session.register_user(user, callback_registration_submitted);
+  }
+
+  /////////////////////////////////////// initialization
+
   $omega_session.on_session_validated(show_logout_controls);
   $omega_session.on_invalid_session(show_login_controls);
 
@@ -98,4 +124,8 @@ $(document).ready(function(){
 
   $('#register_link').live('click', function(event){ show_register_dialog(); });
   $('#register_button').live('click', function(event){ submit_register_dialog(); });
+}
+
+$(document).ready(function(){ 
+  $omega_navigation = OmegaNavigationContainer();
 });
