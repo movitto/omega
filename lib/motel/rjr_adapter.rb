f62002c1daf93e977223e67aadbac67fc8ba10b0
Mo Morsi <mo@morsi.org>
2013-04-12 07:58:33 -0400

    small fix to location initialization order

--- lib/motel/rjr_adapter.rb
+++ lib/motel/rjr_adapter.rb
@@ -72,6 +72,17 @@ class RJRAdapter
 
        raise ArgumentError, "#{new_location} must be a location" unless new_location.is_a?(Motel::Location)
 
+       # TODO remove this initialization from here (or at least some of it)
+       new_location.x = 0 unless new_location.x.is_a?(Integer) || new_location.x.is_a?(Float)
+       new_location.y = 0 unless new_location.y.is_a?(Integer) || new_location.y.is_a?(Float)
+       new_location.z = 0 unless new_location.z.is_a?(Integer) || new_location.z.is_a?(Float)
+       new_location.movement_strategy = Motel::MovementStrategies::Stopped.instance unless new_location.movement_strategy.kind_of?(Motel::MovementStrategy)
+
+       new_location.movement_callbacks  = []
+       new_location.proximity_callbacks = []
+       new_location.children = []
+
+
        unless new_location.parent_id.nil?
          # if parent.nil? throw error?
          parent = Runner.instance.locations.find { |loc| loc.id == new_location.parent_id }
@@ -80,17 +91,6 @@ class RJRAdapter
            new_location.parent = parent
          }
        end
-
-       Motel::Runner.instance.safely_run {
-         new_location.x = 0 unless new_location.x.is_a?(Integer) || new_location.x.is_a?(Float)
-         new_location.y = 0 unless new_location.y.is_a?(Integer) || new_location.y.is_a?(Float)
-         new_location.z = 0 unless new_location.z.is_a?(Integer) || new_location.z.is_a?(Float)
-         new_location.movement_strategy = Motel::MovementStrategies::Stopped.instance unless new_location.movement_strategy.kind_of?(Motel::MovementStrategy)
-
-         new_location.movement_callbacks  = []
-         new_location.proximity_callbacks = []
-         new_location.children = []
-       }
 
        # id gets set here
        # if id exists, throw error? or invoke update_location?
@@ -145,9 +145,8 @@ class RJRAdapter
            }
          end
 
+         # TODO invoke movement/rotation/proximity callbacks as appropriate
        }
-
-       # TODO invoke movement/rotation/proximity callbacks as appropriate
 
        location
     }
