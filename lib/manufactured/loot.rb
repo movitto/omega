e81032bbcd0856691c1a681ab8fb70f53f6274c4
Mo Morsi <mo@morsi.org>
2013-06-15 17:58:26 -0400

    cleanup manufactured::loot and spec

--- lib/manufactured/loot.rb
+++ lib/manufactured/loot.rb
@@ -6,115 +6,34 @@
 require 'cosmos/registry'
 require 'cosmos/entities/solar_system'
 
+require 'manufactured/entity'
+
 module Manufactured
 
 # Free floating groups of items (resources/etc) in a {Cosmos::SolarSystem}
 # which {Manufactured::Ship}s can retrieve if within collection_distance
 class Loot
+  include Manufactured::Entity::InSystem
+  include Manufactured::Entity::HasCargo
+
   # Unique string id of the loot
   attr_accessor :id
-
-  # [Motel::Location] of the loot in its parent solar system
-  attr_reader :location
-
-  # Set location of ship in its parent solar system
-  #
-  # Will set the parent of the specified location to correspond to the solar system's location object
-  # @param [Motel::Location] val location to assign to the ship
-  def location=(val)
-    old_location = @location
-    @location = val
-    unless parent.nil? || @location.nil?
-      @location.parent = parent.location
-
-      @location.parent.remove_child(old_location)
-      @location.parent.add_child(@location)
-    end
-  end
-
-  # [Cosmos::SolarSystem] the ship is in
-  attr_reader :solar_system
-
-  # [String] name of the solar system.
-  #
-  # Used to reference the solar_system w/out having to pass
-  # the entire system around
-  attr_accessor :system_name
-
-  # Set solar system the ship is in
-  #
-  # Assigns the parent of the ship's location to the location corresponding to the new solar system
-  # @param [Cosmos::SolarSystem] val solar system parent to assign to the ship
-  def solar_system=(val)
-    @solar_system = val
-    return if @solar_system.nil?
-    @system_name  = @solar_system.name
-    @location.parent = parent.location unless parent.nil? || @location.nil?
-  end
-
-  # Resources Loot Contains
-  attr_reader :resources
-
-  # Add resource to loot
-  def add_resource(resource_id, quantity)
-    @resources[resource_id] ||= 0
-    @resources[resource_id]  += quantity
-  end
-
-  # Remove specified quantity of resource specified by id from loot
-  #
-  # @param [String] resource_id id of resource being removed
-  # @param [Integer] quantity amount of resource to remove
-  # @raise [Omega::OperationError] if loot does not have the specified quantity of resource
-  def remove_resource(resource_id, quantity)
-    unless @resources.has_key?(resource_id) && @resources[resource_id] >= quantity
-      raise Omega::OperationError, "loot does not contain specified quantity of resource" 
-    end
-    @resources[resource_id] -= quantity
-    @resources.delete(resource_id) if @resources[resource_id] <= 0
-  end
-
-  # Return total quantity of resources stored locally
-  #
-  # @return [Integer] quantity total quantity currently stored in loot
-  def quantity
-    tq = 0
-    @resources.each { |r,q| tq += q }
-    tq
-  end
-
-  # Return boolean indicating if loot is empty
-  def empty?
-    @resources.empty?
-  end
 
   # Loot initializer
   # @param [Hash] args hash of options to initialize loot with
   # @option args [String] :id,'id' id to assign to the loot
   # @option args [Motel::Location] :location,'location' location of the loot in the solar system
   def initialize(args = {})
-    @id        = args['id']       || args[:id]
-    @resources = args['resources']|| args[:resources] || {}
+    args[:location] =
+      Motel::Location.new :coordinates => [0,0,1],
+                          :orientation => [1,0,0]  unless args.has_key?(:location) ||
+                                                          args.has_key?('location')
 
-    if args.has_key?('solar_system') || args.has_key?(:solar_system)
-      self.solar_system = args['solar_system'] || args[:solar_system]
-    elsif args.has_key?('system_name') || args.has_key?(:system_name)
-      @system_name = args['system_name'] || args[:system_name]
-      # TODO would rather not access the cosmos registry directly here
-      solar_system= Cosmos::Registry.instance.find_entity(:type => :solarsystem,
-                                                          :name => @system_name)
-      self.solar_system = solar_system unless solar_system.nil?
-    end
-
-    # location should be set after solar system so parent is set correctly
-    self.location = args['location'] || args[:location]
-
-    self.location = Motel::Location.new if @location.nil?
-    @location.x = 0 if @location.x.nil?
-    @location.y = 0 if @location.y.nil?
-    @location.z = 0 if @location.z.nil?
-
-    self.location.movement_strategy = Motel::MovementStrategies::Stopped.instance
+    attr_from_args args, :id                   => nil,
+                         :resources            =>  [],
+                         :location             => nil,
+                         :system_id            => nil,
+                         :solar_system         => nil
   end
 
   # Return boolean indicating if this loot is valid
@@ -129,22 +48,11 @@ class Loot
   # * solar system is set to Cosmos::SolarSystem
   def valid?
     !@id.nil? && @id.is_a?(String) && @id != "" &&
+
     !@location.nil? && @location.is_a?(Motel::Location) &&
      @location.movement_strategy == Motel::MovementStrategies::Stopped.instance &&
-    !@solar_system.nil? && @solar_system.is_a?(Cosmos::SolarSystem)
-  end
 
-  # Return loots's parent solar system
-  #
-  # @return [Cosmos::SolarSystem]
-  def parent
-    return self.solar_system
-  end
-
-  # Set loots's parent solar system
-  # @param [Cosmos::SolarSystem] system solar system to assign to loots
-  def parent=(system)
-    self.solar_system = system
+    !@solar_system.nil? && @solar_system.is_a?(Cosmos::Entities::SolarSystem)
   end
 
   # Convert loot to json representation and return it
@@ -154,7 +62,7 @@ class Loot
       'data'       =>
         {:id => id,
          :location => @location,
-         :system_name => (@solar_system.nil? ? @system_name : @solar_system.name),
+         :system_id => @system_id,
          :resources => @resources }
     }.to_json(*a)
   end
--- spec/manufactured/loot_spec.rb
+++ spec/manufactured/loot_spec.rb
@@ -4,162 +4,116 @@
 # Licensed under the AGPLv3+ http://www.gnu.org/licenses/agpl.txt
 
 require 'spec_helper'
+require 'manufactured/loot'
 
-describe Manufactured::Loot do
+module Manufactured
+describe Loot do
+  describe "#initialize" do
+    it "sets defaults" do
+      l = Loot.new
+      l.id.should be_nil
+      l.resources.should == []
+      l.solar_system.should be_nil
+      l.system_id.should be_nil
 
-  it "should successfully accept and set loot params" do
-     sys  = Cosmos::SolarSystem.new :name => 'system1'
-     sys2 = Cosmos::SolarSystem.new :name => 'system2'
-     loot = Manufactured::Loot.new :id => 'loot1',
-                                   :solar_system => sys
-                                   
-     loot.id.should == 'loot1'
-     loot.location.should_not be_nil
-     loot.location.x.should == 0
-     loot.location.y.should == 0
-     loot.location.z.should == 0
+      l.location.should be_an_instance_of(Motel::Location)
+      l.location.coordinates.should == [0,0,1]
+      l.location.orientation.should == [1,0,0]
+    end
 
-     loot.parent.should == sys
-     loot.parent = sys2
-     loot.parent.should == sys2
+    it "sets attributes" do
+      r = build(:resource)
+      sys = build(:solar_system)
+      loc = build(:location)
+      l = Loot.new :id                  => 'loot1',
+                   :resources           => [r],
+                   :solar_system        => sys,
+                   :location            =>   loc
+
+      l.id.should == 'loot1'
+      l.resources.should == [r]
+      l.solar_system.should == sys
+      l.system_id.should == sys.id
+      l.location.should == loc
+    end
   end
 
-  it "should lookup parent system in registry if name given" do
-     sys  = Cosmos::SolarSystem.new :name => 'system1'
-     gal  = Cosmos::Galaxy.new :name => 'galaxy1', :solar_systems => [sys]
-     Cosmos::Registry.instance.add_child gal
+  describe "#valid?" do
+    before(:each) do
+      @sys = build(:solar_system)
+      @l   = Loot.new :id           => 'loot1',
+                      :solar_system => @sys
+    end
 
-     loot = Manufactured::Loot.new :id => 'loot1', :system_name => 'system1'
-     loot.solar_system.should == sys
-  end
-
-  it "should verify validity of loot" do
-    sys = Cosmos::SolarSystem.new
-    loot = Manufactured::Loot.new :id => 'loot1', :solar_system => sys
-    loot.valid?.should be_true
-
-    loot.id = nil
-    loot.valid?.should be_false
-    loot.id = 'ship1'
-
-    loot.location = nil
-    loot.valid?.should be_false
-    loot.location = Motel::Location.new :x => 0, :y => 0, :z => 0
-
-    loot.location.movement_strategy = Motel::MovementStrategies::Linear.new(:step_delay => 1, :speed => 5)
-    loot.valid?.should be_false
-    loot.location.movement_strategy = Motel::MovementStrategies::Stopped.instance
-
-    loot.solar_system = nil
-    loot.valid?.should be_false
-    loot.solar_system = sys
-
-    loot.valid?.should be_true
-  end
-
-  it "should set parent location when setting location" do
-    sys1 = Cosmos::SolarSystem.new :location => Motel::Location.new(:id => 1)
-    oloc = Motel::Location.new
-    nloc = Motel::Location.new
-    loot = Manufactured::Loot.new :id => 'loot1', :solar_system => sys1, :location => oloc
-    loot.location = nloc
-    nloc.parent.should == sys1.location
-    sys1.location.children.should include(nloc)
-    sys1.location.children.should_not include(oloc)
-  end
-
-  it "should set parent location when setting system" do
-    sys1 = Cosmos::SolarSystem.new :location => Motel::Location.new(:id => 1)
-    sys2 = Cosmos::SolarSystem.new :location => Motel::Location.new(:id => 1)
-    loot = Manufactured::Loot.new :id => 'loot1', :solar_system => sys1
-    loot.location.parent.should == sys1.location
-    loot.solar_system = sys2
-    loot.location.parent.should == sys2.location
-  end
-
-  it "should permit storing resources locally" do
-    loot   = Manufactured::Loot.new :id => 'loot1'
-    loot.resources.should be_empty
-    loot.should be_empty
+    it "returns true" do
+      @l.should be_valid
+    end
     
-    res = Cosmos::Resource.new :name => 'titanium', :type => 'metal'
-    loot.add_resource res.id, 10
-    loot.should_not be_empty
-    loot.resources.should_not be_empty
-    loot.resources.size.should == 1
-    loot.resources[res.id].should == 10
-    loot.quantity.should == 10
+    context "id is invalid" do
+      it "returns false" do
+        @l.id = nil
+        @l.should_not be_valid
+      end
+    end
 
-    loot.add_resource res.id, 60
-    loot.resources.size.should == 1
-    loot.resources[res.id].should == 70
-    loot.quantity.should == 70
+    context "location is invalid" do
+      it "returns false" do
+        @l.location = nil
+        @l.should_not be_valid
+      end
+    end
 
-    loot.remove_resource res.id, 40
-    loot.resources.size.should == 1
-    loot.resources[res.id].should == 30
-    loot.quantity.should == 30
+    context "movement strategy is invalid" do
+      it "returns false" do
+        @l.location.movement_strategy = nil
+        @l.should_not be_valid
+      end
+    end
 
-    # should remove resource if set to 0
-    loot.remove_resource res.id, 30
-    loot.resources.size.should == 0
-    loot.quantity.should == 0
-    loot.should be_empty
+    context "solar system is invalid" do
+      it "returns false" do
+        @l.solar_system = nil
+        @l.should_not be_valid
+      end
+    end
   end
 
-  it "should raise error if cannot or remove resource" do
-    loot   = Manufactured::Loot.new :id => 'loot1'
-    loot.resources.should be_empty
-    loot.should be_empty
+  describe "#to_json" do
+    it "returns loot in json format" do
+      system1 = Cosmos::Entities::SolarSystem.new :id => 'system1'
+      location= Motel::Location.new :id => 20, :y => -15
+      res1    = build(:resource)
 
-    res = Cosmos::Resource.new :name => 'titanium', :type => 'metal'
-    lambda{
-      loot.remove_resource res.id, 1
-    }.should raise_error(Omega::OperationError)
+      l = Manufactured::Loot.new(:id => 'loot1', :solar_system => system1, 
+                                 :location => location,
+                                 :resources => {res1.id => 100})
+
+      j = l.to_json
+      j.should include('"json_class":"Manufactured::Loot"')
+      j.should include('"id":"loot1"')
+      j.should include('"'+res1.id+'":100')
+      j.should include('"json_class":"Motel::Location"')
+      j.should include('"id":20')
+      j.should include('"y":-15')
+      j.should include('"system_id":"system1"')
+    end
   end
 
-  it "should return boolean indicating if loot is empty" do
-    loot   = Manufactured::Loot.new :id => 'loot1'
-    loot.should be_empty
+  describe "#json_create" do
+    it "returns loot from json format" do
+      j = '{"json_class":"Manufactured::Loot","data":{"id":"loot1","location":{"json_class":"Motel::Location","data":{"id":20,"x":0,"y":-15.0,"z":0,"restrict_view":true,"restrict_modify":true,"parent_id":null,"children":[],"movement_strategy":{"json_class":"Motel::MovementStrategies::Stopped","data":{"step_delay":1}},"movement_callbacks":[],"proximity_callbacks":[]}},"system_id":"system1","resources":{"metal-titanium":100}}}'
+      l = JSON.parse(j)
 
-    res1 = Cosmos::Resource.new :name => 'titanium', :type => 'metal'
-    res2 = Cosmos::Resource.new :name => 'steel',    :type => 'metal'
-    loot.add_resource res1.id, 20
-    loot.add_resource res1.id, 30
-    loot.should_not be_empty
+      l.class.should == Manufactured::Loot
+      l.id.should == "loot1"
+      l.resources.size.should == 1
+      l.resources.first.first.should == "metal-titanium"
+      l.resources.first.last.should == 100
+      l.location.should_not be_nil
+      l.location.y.should == -15
+      l.system_id.should == "system1"
+    end
   end
 
-  it "should be convertable to json" do
-    system1 = Cosmos::SolarSystem.new :name => 'system1'
-    location= Motel::Location.new :id => 20, :y => -15
-    res1    = Cosmos::Resource.new :name => 'titanium', :type => 'metal'
-
-    l = Manufactured::Loot.new(:id => 'loot1', :solar_system => system1, 
-                               :location => location,
-                               :resources => {res1.id => 100})
-
-    j = l.to_json
-    j.should include('"json_class":"Manufactured::Loot"')
-    j.should include('"id":"loot1"')
-    j.should include('"'+res1.id+'":100')
-    j.should include('"json_class":"Motel::Location"')
-    j.should include('"id":20')
-    j.should include('"y":-15')
-    j.should include('"system_name":"system1"')
-  end
-
-  it "should be convertable from json" do
-    j = '{"json_class":"Manufactured::Loot","data":{"id":"loot1","location":{"json_class":"Motel::Location","data":{"id":20,"x":0,"y":-15.0,"z":0,"restrict_view":true,"restrict_modify":true,"parent_id":null,"children":[],"movement_strategy":{"json_class":"Motel::MovementStrategies::Stopped","data":{"step_delay":1}},"movement_callbacks":[],"proximity_callbacks":[]}},"system_name":"system1","resources":{"metal-titanium":100}}}'
-    l = JSON.parse(j)
-
-    l.class.should == Manufactured::Loot
-    l.id.should == "loot1"
-    l.resources.size.should == 1
-    l.resources.first.first.should == "metal-titanium"
-    l.resources.first.last.should == 100
-    l.location.should_not be_nil
-    l.location.y.should == -15
-    l.system_name.should == "system1"
-  end
-
-end
+end # describe Loot
+end # module Manufactured
