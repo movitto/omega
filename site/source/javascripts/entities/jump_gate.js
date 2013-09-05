eb217bf45d0482cbbe69c8e254c65397af35cca4
Mo Morsi <mo@morsi.org>
2013-09-04 20:24:51 -0400

    reorganize js jump gate entity, add some more graphical features

--- site/source/javascripts/entities/jump_gate.js
+++ site/source/javascripts/entities/jump_gate.js
@@ -12,54 +12,100 @@ function JumpGate(args){
 
   //this.id = this.solar_system + '-' + this.endpoint;
 
-  this.json_class = 'Cosmos::Entities::JumpGate';
   var jg = this;
+  this.json_class = 'Cosmos::Entities::JumpGate';
 
+  // convert location
   this.location = new Location(this.location);
 
   // instantiate mesh to draw gate on canvas
-  this.create_mesh = function(){
-    if(this.mesh_geometry == null) return;
-    this.mesh =
-      UIResources().cached("jump_gate_" + this.id + "_mesh",
-        function(i) {
-          var mesh = new THREE.Mesh(jg.mesh_geometry, jg.mesh_material);
-          mesh.position.x = jg.location.x;
-          mesh.position.y = jg.location.y;
-          mesh.position.z = jg.location.z;
+  _jump_gate_load_mesh(this);
+  this.create_mesh = _jump_gate_create_mesh;
+  this.create_mesh();
 
-          var offset = $omega_config.resources['jump_gate'].offset;
-          if(offset){
-            mesh.position.x += offset[0];
-            mesh.position.y += offset[1];
-            mesh.position.z += offset[2];
-          }
+  // instantiate lights to draw on gate
+  _jump_gate_load_lights(this);
 
-          var scale = $omega_config.resources['jump_gate'].scale;
-          if(scale){
-            mesh.scale.x = scale[0];
-            mesh.scale.y = scale[1];
-            mesh.scale.z = scale[2];
-          }
+  // instantiate particle effects to draw on gate
+  _jump_gate_load_effects(this);
 
-          var rotation = $omega_config.resources['jump_gate'].rotation;
-          if(rotation){
-            mesh.rotation.x = rotation[0];
-            mesh.rotation.y = rotation[1];
-            mesh.rotation.z = rotation[2];
-            mesh.matrix.setRotationFromEuler(mesh.rotation);
-          }
+  // instantiate sphere to draw around jump_gate on canvas
+  _jump_gate_load_selection_sphere(this);
 
-          return mesh;
-        });
+  // some text to render in details box on click
+  this.details = _jump_gate_render_details;
 
-    this.clickable_obj = this.mesh;
-    this.components.push(this.mesh);
-
-    // reload entity if already in scene
-    if(this.current_scene) this.current_scene.reload_entity(this);
+  /* added_to scene callback
+   */
+  this.added_to = function(scene){
+    this.current_scene = scene;
   }
 
+  /* clicked_in scene callback
+   */
+  this.clicked_in = _jump_gate_clicked_in;
+
+  /* unselected in scene callback
+   */
+  this.unselected_in = _jump_gate_unselected_in;
+
+  /* removed_from scene callback
+   */
+  this.removed_from = function(scene){
+    this.current_scene = null;
+  }
+}
+
+///////////////////////// private helper / utility methods
+
+/* JumpGate::create_mesh method
+ */
+function _jump_gate_create_mesh(){
+  var jg = this;
+  if(this.mesh_geometry == null) return;
+  this.mesh =
+    UIResources().cached("jump_gate_" + this.id + "_mesh",
+      function(i) {
+        var mesh = new THREE.Mesh(jg.mesh_geometry, jg.mesh_material);
+        mesh.position.x = jg.location.x;
+        mesh.position.y = jg.location.y;
+        mesh.position.z = jg.location.z;
+ 
+        var offset = $omega_config.resources['jump_gate'].offset;
+        if(offset){
+          mesh.position.x += offset[0];
+          mesh.position.y += offset[1];
+          mesh.position.z += offset[2];
+        }
+ 
+        var scale = $omega_config.resources['jump_gate'].scale;
+        if(scale){
+          mesh.scale.x = scale[0];
+          mesh.scale.y = scale[1];
+          mesh.scale.z = scale[2];
+        }
+ 
+        var rotation = $omega_config.resources['jump_gate'].rotation;
+        if(rotation){
+          mesh.rotation.x = rotation[0];
+          mesh.rotation.y = rotation[1];
+          mesh.rotation.z = rotation[2];
+          mesh.matrix.setRotationFromEuler(mesh.rotation);
+        }
+ 
+        return mesh;
+      });
+
+  this.clickable_obj = this.mesh;
+  this.components.push(this.mesh);
+
+  // reload entity if already in scene
+  if(this.current_scene) this.current_scene.reload_entity(this);
+}
+
+/* Helper to load jump gate mesh resources
+ */
+function _jump_gate_load_mesh(jg){
   var mesh_texture =
     UIResources().cached("jump_gate_mesh_texture",
       function(i) {
@@ -72,13 +118,13 @@ function JumpGate(args){
         return texture;
       });
 
-  this.mesh_material =
+  jg.mesh_material =
     UIResources().cached("jump_gate_mesh_material",
       function(i) {
         return new THREE.MeshBasicMaterial( { map: mesh_texture } );
       });
 
-  this.mesh_geometry =
+  jg.mesh_geometry =
     UIResources().cached('jump_gate_mesh_geometry',
       function(i) {
         var path = UIResources().images_path + $omega_config.resources['jump_gate']['geometry'];
@@ -89,14 +135,114 @@ function JumpGate(args){
         })
         return null;
       });
+}
 
-  this.create_mesh();
-
-  // instantiate sphere to draw around jump_gate on canvas
+/* Helper to load jump gate lights
+ */
+function _jump_gate_load_lights(jg){
   var sphere_geometry =
-    UIResources().cached('jump_gate_' + this.trigger_distance + '_container_geometry',
+    UIResources().cached('jump_gate_light1_geo',
       function(i) {
-        var radius    = this.trigger_distance, segments = 32, rings = 32;
+        return new THREE.SphereGeometry(10, 32, 32);
+      });
+
+  var sphere_material =
+    UIResources().cached('jump_gate_light1_mat',
+      function(i) {
+        return new THREE.MeshBasicMaterial({color: 0xff0000});
+      });
+
+  jg.light1 =
+    UIResources().cached('jump_gate_light1_mesh',
+      function(i) {
+        return new THREE.Mesh(sphere_geometry, sphere_material);
+      });
+
+  jg.light1.position.set(-22,-15,175)
+  jg.light1_dir = false;
+  jg.light1.update_particles = function(){
+    var c = sphere_material.color.getHex();
+    if(jg.light1_dir)
+      sphere_material.color.setHex(c - 0x100000)
+    else
+      sphere_material.color.setHex(c + 0x100000)
+
+    if(sphere_material.color.getHex() > 0xff0000){
+      sphere_material.color.setHex(0xff0000);
+      jg.light1_dir = !jg.light1_dir;
+    }else if(sphere_material.color.getHex() < 0x000000){
+      sphere_material.color.setHex(0x000000);
+      jg.light1_dir = !jg.light1_dir;
+    }
+  }
+
+  jg.components.push(jg.light1);
+}
+
+/* Helper to load jump gate effects
+ */
+function _jump_gate_load_effects(jg){
+  var plane = 20, lifespan = 200;
+  var pMaterial =
+    UIResources().cached('jump_gate_effect1_mat',
+      function(i) {
+        return new THREE.ParticleBasicMaterial({
+         color: 0x0000FF, size: 20,
+         map: UIResources().load_texture("images/particle.png"),
+         blending: THREE.AdditiveBlending, transparent: true });
+       });
+
+  var particles = 
+    UIResources().cached('jump_gate_effect1_geo',
+      function(i) {
+        var geo = new THREE.Geometry();
+        for(var i = 0; i < plane; ++i){
+          for(var j = 0; j < plane; ++j){
+            var pv = new THREE.Vector3(i, j, 0);
+            pv.velocity = Math.random();
+            pv.lifespan = lifespan;
+            pv.moving = false;
+            geo.vertices.push(pv)
+          }
+        }
+        return geo;
+      });
+
+  jg.effects1 = new THREE.ParticleSystem(particles, pMaterial);
+  jg.effects1.position.set(-30,-25,75)
+  jg.effects1.sortParticles = true;
+
+  jg.effects1.update_particles = function(){
+    var p = plane*plane;
+    var not_moving = [];
+    while(p--){
+      var pv = this.geometry.vertices[p]
+      if(pv.moving){
+        pv.z -= pv.velocity;
+        pv.lifespan -= 1;
+        if(pv.lifespan < 0){
+          pv.z = 0;
+          pv.lifespan = 200;
+          pv.moving = false;
+        }
+      }else{
+        not_moving.push(pv);
+      }
+    }
+    not_moving[Math.floor(Math.random()*(not_moving.length-1))].moving = true;
+    this.geometry.__dirtyVertices = true;
+  }
+
+  jg.components.push(jg.effects1);
+}
+
+/* Helper to load jump gate selection sphere
+ */
+function _jump_gate_load_selection_sphere(jg){
+  var sphere_geometry =
+    UIResources().cached('jump_gate_' + jg.trigger_distance + '_container_geometry',
+      function(i) {
+        var radius    = jg.trigger_distance, segments = 32, rings = 32;
         return new THREE.SphereGeometry(radius, segments, rings);
       });
 
@@ -109,8 +255,8 @@ function JumpGate(args){
 
       });
 
-  this.sphere =
-    UIResources().cached("jump_gate_" + this.id + "_container",
+  jg.sphere =
+    UIResources().cached("jump_gate_" + jg.id + "_container",
                          function(i) {
                            var sphere = new THREE.Mesh(sphere_geometry, sphere_material);
                            sphere.position.x = jg.location.x;
@@ -119,53 +265,40 @@ function JumpGate(args){
                            sphere.scale.x = sphere.scale.y = sphere.scale.z = 5;
                            return sphere;
                          });
+}
 
-  // some text to render in details box on click
-  this.details = function(){
-    return ['Jump Gate to ' + this.endpoint_id + '<br/>',
-            '@ ' + this.location.to_s() + "<br/><br/>",
-            "<span class='commands' id='cmd_trigger_jg'>Trigger</div>"];
-  }
+/* JumpGate::details method
+ */
+function _jump_gate_render_details(){
+  return ['Jump Gate to ' + this.endpoint_id + '<br/>',
+          '@ ' + this.location.to_s() + "<br/><br/>",
+          "<span class='commands' id='cmd_trigger_jg'>Trigger</div>"];
+}
 
-  /* added_to scene callback
-   */
-  this.added_to = function(scene){
-    this.current_scene = scene;
-  }
+/* JumpGate::clicked_in method
+ */
+function _jump_gate_clicked_in(scene){
+  this.selected = true;
 
-  /* clicked_in scene callback
-   */
-  this.clicked_in = function(scene){
-    this.selected = true;
+  $('#cmd_trigger_jg').die();
+  $('#cmd_trigger_jg').live('click', function(e){
+    Commands.trigger_jump_gate(jg);
+  });
 
-    $('#cmd_trigger_jg').die();
-    $('#cmd_trigger_jg').live('click', function(e){
-      Commands.trigger_jump_gate(jg);
-    });
+  if(this.components.indexOf(this.sphere) == -1)
+    this.components.push(this.sphere);
+  this.clickable_obj = this.sphere;
+  scene.reload_entity(this);
+}
 
-    if(this.components.indexOf(this.sphere) == -1)
-      this.components.push(this.sphere);
-    this.clickable_obj = this.sphere;
-    scene.reload_entity(this);
-  }
+/* JumpGate::unselected_in method
+ */
+function _jump_gate_unselected_in(scene){
+  this.selected = false;
 
-  /* unselected in scene callback
-   */
-  this.unselected_in = function(scene){
-    this.selected = false;
-
-    scene.reload_entity(this, function(s,e){
-      var si = e.components.indexOf(e.sphere);
-      if(si != -1) e.components.splice(si, 1);
-      e.clickable_obj = e.mesh;
-    });
-  }
-
-  /* removed_from scene callback
-   */
-  this.removed_from = function(scene){
-    this.current_scene = null;
-
-  }
-
+  scene.reload_entity(this, function(s,e){
+    var si = e.components.indexOf(e.sphere);
+    if(si != -1) e.components.splice(si, 1);
+    e.clickable_obj = e.mesh;
+  });
 }
