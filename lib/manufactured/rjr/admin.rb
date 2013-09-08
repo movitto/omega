9cca63b35f03833a3453cd72e686569361950866
Mo Morsi <mo@morsi.org>
2013-09-08 16:34:42 -0400

    add an admin mode to the server enabling additional administrative methods
      these are accessible to all so this is disabled by default / should
      not be enabled in live production

---
+++ lib/manufactured/rjr/admin.rb
@@ -0,0 +1,34 @@
+# manufactured rjr admin module
+#
+# Only included in admin mode
+#
+# Copyright (C) 2013 Mohammed Morsi <mo@morsi.org>
+# Licensed under the Apache License, Version 2.0
+
+# TODO require superadmin role to run these methods
+#      (role itself, not any privs specifically)
+
+# unbridled entity update mechanism
+admin_set = proc { |entity|
+  registry.update entity, &with_id(entity.id)
+  nil
+}
+
+# unbridled entity callback execution mechanism
+admin_run_callbacks = proc { |entity_id, *args|
+  registry.safe_exec { |entities|
+    e = entities.find { |e| e.id == entity_id }
+    e.run_callbacks *args
+  }
+  nil
+}
+
+ADMIN_METHODS = { :set   => admin_set, :run_callbacks => admin_run_callbacks }
+
+def dispatch_manufactured_rjr_admin(dispatcher)
+  dispatcher.handle "manufactured::admin::set", 
+                     &ADMIN_METHODS[:set]
+
+  dispatcher.handle "manufactured::admin::run_callbacks",
+                     &ADMIN_METHODS[:run_callbacks]
+end
---
+++ spec/manufactured/rjr/admin_spec.rb
@@ -0,0 +1,56 @@
+# manufactured::admin test
+#
+# Copyright (C) 2013 Mohammed Morsi <mo@morsi.org>
+# Licensed under the AGPLv3+ http://www.gnu.org/licenses/agpl.txt
+
+require 'spec_helper'
+require 'manufactured/rjr/admin'
+require 'rjr/dispatcher'
+
+module Manufactured::RJR
+  describe "#admin::set" do
+    include Omega::Server::DSL # for with_id below
+
+    before(:each) do
+      setup_manufactured :ADMIN_METHODS
+    end
+
+    it "updates the specified registry entity" do
+      e = create(:valid_ship)
+      e.hp = 0
+      Manufactured::RJR.registry.should_receive(:update).with(e).
+                                 and_call_original
+      @s.set e
+
+      Manufactured::RJR.registry.entity(&with_id(e.id)).hp.should == 0
+    end
+  end # describe "#admin::set"
+
+  describe "#admin::run_callbacks" do
+    before(:each) do
+      setup_manufactured :ADMIN_METHODS
+    end
+
+    it "runs callbacks on the registry entity specified by id" do
+      e = create(:valid_ship)
+      re = Manufactured::RJR.registry.safe_exec { |es| es.find { |e| e.id == e.id }}
+      re.should_receive(:run_callbacks).with(42)
+      @s.run_callbacks e.id, 42
+    end
+  end # describe "#admin::run_callbacks"
+
+  describe "#dispatch_manufactured_rjr_admin" do
+    it "adds manufactured::admin::set to dispatcher" do
+      d = ::RJR::Dispatcher.new
+      dispatch_manufactured_rjr_admin(d)
+      d.handlers.keys.should include("manufactured::admin::set")
+    end
+
+    it "adds manufactured::admin::run_callbacks to dispatcher" do
+      d = ::RJR::Dispatcher.new
+      dispatch_manufactured_rjr_admin(d)
+      d.handlers.keys.should include("manufactured::admin::run_callbacks")
+    end
+  end
+
+end #module Users::RJR
