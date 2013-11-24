969a2a30c44fd14a8189c13b2e268be95038f95d
Mo Morsi <mo@morsi.org>
2013-11-24 07:37:56 -0500

    Render system interconnections in refactored UI

--- site2/source/javascripts/omega/solar_system.js
+++ site2/source/javascripts/omega/solar_system.js
@@ -8,6 +8,8 @@ Omega.SolarSystem = function(parameters){
   this.components = [];
   this.shader_components = [];
 
+  this.interconnections = [];
+
   this.children   = [];
   this.background = '';
   $.extend(this, parameters);
@@ -52,6 +54,17 @@ Omega.SolarSystem.prototype = {
 
     /// text
       Omega.SolarSystem.gfx.text_material = new THREE.MeshBasicMaterial({ color: 0x3366FF, overdraw: true });
+
+    /// interconnections
+      Omega.SolarSystem.gfx.interconn_material = new THREE.LineBasicMaterial({color: 0xF80000});
+
+      var texture_path = config.url_prefix + config.images_path + '/particle.png';
+      var texture = THREE.ImageUtils.loadTexture(texture_path, {}, event_cb);
+      Omega.SolarSystem.gfx.interconn_particle_material =
+        new THREE.ParticleBasicMaterial({
+          color: 0xFF0000, size: 75, transparent: true ,
+          map: texture, blending: THREE.AdditiveBlending
+        });
   },
 
   init_gfx : function(config, event_cb){
@@ -69,14 +82,60 @@ Omega.SolarSystem.prototype = {
     var text_geo = new THREE.TextGeometry(this.name, Omega.SolarSystem.prototype.text_opts);
     this.text    = new THREE.Mesh(text_geo, Omega.SolarSystem.gfx.text_material);
     if(this.location) this.text.position.set(this.location.x, this.location.y, this.location.z - 50);
+
+    this.components = [this.mesh, this.plane, this.text];
   },
 
   run_effects : function(){
-    /// TODO run jump gate line effects
+    for(var i = 0; i < this.interconnections.length; i++){
+      var interconn = this.interconnections[i];
+      var v         = interconn.geometry.vertices[0];
+      var endpoint  = interconn.endpoint;
+
+      var d  = this.location.distance_from(endpoint.location.x,
+                                           endpoint.location.y,
+                                           endpoint.location.z);
+      var dx = (endpoint.location.x - this.location.x) / d;
+      var dy = (endpoint.location.y - this.location.y) / d;
+      var dz = (endpoint.location.z - this.location.z) / d;
+
+      v.set(interconn.ticker * dx * 50,
+            interconn.ticker * dy * 50,
+            interconn.ticker * dz * 50)
+
+      interconn.ticker += 1;
+      if(endpoint.location.distance_from(this.location.x+v.x,
+                                         this.location.y+v.y,
+                                         this.location.z+v.z) < 100)
+        interconn.ticker = 0;
+      interconn.geometry.__dirtyVertices = true;
+    }
   },
 
-  add_jump_gate : function(jg){
-    /// TODO
+  add_interconn : function(endpoint){
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(this.location.x,
+                                             this.location.y,
+                                             this.location.z));
+    geometry.vertices.push(new THREE.Vector3(endpoint.location.x,
+                                             endpoint.location.y,
+                                             endpoint.location.z));
+    var line = new THREE.Line(geometry, Omega.SolarSystem.gfx.interconn_material);
+
+    var particle_geo = new THREE.Geometry();
+    particle_geo.vertices.push(new THREE.Vector3(0,0,0));
+    var particle_system = new THREE.ParticleSystem(particle_geo,
+              Omega.SolarSystem.gfx.interconn_particle_material);
+    particle_system.position.set(this.location.x,
+                                 this.location.y,
+                                 this.location.z);
+    particle_system.sortParticles = true;
+    particle_system.ticker = 0;
+    particle_system.endpoint = endpoint;
+
+    this.components.push(line);
+    this.components.push(particle_system);
+    this.interconnections.push(particle_system);
   }
 };
 
--- site2/tests/tests/omega/solar_system.js
+++ site2/tests/tests/omega/solar_system.js
@@ -72,6 +72,18 @@ describe("Omega.SolarSystem", function(){
       solar_system.init_gfx();
       assert(solar_system.mesh).equals(mesh);
     });
+    
+    it("sets omege_entity on mesh", function(){
+      var solar_system = new Omega.SolarSystem();
+      solar_system.init_gfx();
+      assert(solar_system.mesh.omega_entity).equals(solar_system);
+    });
+
+    it("sets mesh position", function(){
+      var solar_system = new Omega.SolarSystem({location : {x: 50, y: 60, z: -75}});
+      solar_system.init_gfx();
+      assert(solar_system.plane.position.toArray()).isSameAs([50, 60, -75]);
+    });
 
     it("clones SolarSystem plane", function(){
       var solar_system = new Omega.SolarSystem();
@@ -81,6 +93,12 @@ describe("Omega.SolarSystem", function(){
       assert(solar_system.plane).equals(mesh);
     });
 
+    it("sets plane position", function(){
+      var solar_system = new Omega.SolarSystem({location : {x: 50, y: 60, z: -75}});
+      solar_system.init_gfx();
+      assert(solar_system.plane.position.toArray()).isSameAs([50, 60, -75]);
+    });
+
     it("creates text for solar system", function(){
       var solar_system = new Omega.SolarSystem();
       solar_system.init_gfx();
@@ -88,12 +106,52 @@ describe("Omega.SolarSystem", function(){
       assert(solar_system.text.geometry).isOfType(THREE.TextGeometry);
       assert(solar_system.text.material).isOfType(THREE.MeshBasicMaterial);
     });
+
+    it("sets text position", function(){
+      var solar_system = new Omega.SolarSystem({location : {x: 50, y: 60, z: -75}});
+      solar_system.init_gfx();
+      assert(solar_system.text.position.toArray()).isSameAs([50, 60, -125]);
+    });
+    
+    it("adds mesh, plane, text to solar system scene components", function(){
+      var solar_system = new Omega.SolarSystem();
+      solar_system.init_gfx();
+      assert(solar_system.components).isSameAs([solar_system.mesh, solar_system.plane, solar_system.text]);
+    });
   });
 
   describe("#run_effects", function(){
+    //it("updates interconnect particles") // NIY
   });
 
-  describe("#add_jump_gate", function(){
+  describe("#add_interconn", function(){
+    var system, endpoint;
+    
+    before(function(){
+      Omega.Test.Canvas.Entities();
+      system   = new Omega.SolarSystem({location : {x:100,y:200,z:300}});
+      endpoint = new Omega.SolarSystem({location : {x:-300,y:-200,z:-100}});
+    });
+
+    it("adds line to solar system scene components", function(){
+      system.add_interconn(endpoint);
+      var line = system.components[0];
+      assert(line).isOfType(THREE.Line);
+      assert(line.geometry.vertices[0].toArray()).isSameAs([100,200,300]);
+      assert(line.geometry.vertices[1].toArray()).isSameAs([-300,-200,-100]);
+      assert(line.material).isOfType(THREE.LineBasicMaterial);
+    });
+
+    it("adds particle system to solar system scene components and interconnects", function(){
+      system.add_interconn(endpoint);
+      var particles = system.components[1];
+      assert(particles).isOfType(THREE.ParticleSystem);
+      assert(particles.material).isOfType(THREE.ParticleBasicMaterial);
+      assert(particles.geometry.vertices.length).equals(1);
+      assert(particles.ticker).equals(0);
+      assert(particles.endpoint).equals(endpoint);
+      assert(system.interconnections).isSameAs([particles]);
+    });
   });
 
   describe("#with_id", function(){
