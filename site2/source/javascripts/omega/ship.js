a2e1afca92b3c4f9fec786dcb4a9d8ef6c439df9
Mo Morsi <mo@morsi.org>
2013-11-24 15:05:56 -0500

    Start flushing out ship gfx in new ui

--- site2/source/javascripts/omega/ship.js
+++ site2/source/javascripts/omega/ship.js
@@ -4,6 +4,8 @@
  *  Licensed under the AGPLv3+ http://www.gnu.org/licenses/agpl.txt
  */
 
+/// TODO trails, mining, attack lines
+
 Omega.Ship = function(parameters){
   this.components = [];
   this.shader_components = [];
@@ -11,7 +13,141 @@ Omega.Ship = function(parameters){
 };
 
 Omega.Ship.prototype = {
-  json_class : 'Manufactured::Ship'
+  json_class : 'Manufactured::Ship',
+
+  highlight_props : {
+    x     :    0, y     : 200, z     : 0,
+    rot_x : 3.14, rot_y :   0, rot_z : 0
+  },
+
+  load_gfx : function(config, event_cb){
+    if(typeof(Omega.Ship.gfx)            === 'undefined') Omega.Ship.gfx = {};
+    if(typeof(Omega.Ship.gfx[this.type]) !== 'undefined') return;
+    Omega.Ship.gfx[this.type] = {};
+
+    var texture_path    = config.url_prefix + config.images_path + config.resources.ships[this.type].material;
+    var geometry_path   = config.url_prefix + config.images_path + config.resources.ships[this.type].geometry;
+    var geometry_prefix = config.url_prefix + config.images_path + config.meshes_path;
+    var rotation        = config.resources.ships[this.type].geometry.rotation;
+    var offset          = config.resources.ships[this.type].geometry.offset;
+    var scale           = config.resources.ships[this.type].geometry.scale;
+
+    //// mesh
+      /// each ship instance should set position of mesh
+      var texture      = THREE.ImageUtils.loadTexture(texture_path, {}, event_cb);
+      var material     = new THREE.MeshLambertMaterial({map: texture, overdraw: true});
+
+      var _this = this;
+      new THREE.JSONLoader().load(geometry_path, function(mesh_geometry){
+        var mesh = new THREE.Mesh(mesh_geometry, material);
+        Omega.Ship.gfx[_this.type].mesh = mesh;
+        if(offset)
+          mesh.position.set(offset[0], offset[1], offset[2]);
+        if(scale)
+          mesh.scale.set(scale[0], scale[1], scale[2]);
+        if(rotation){
+          mesh.rotation.set(rotation[0], rotation[1], rotation[2]);
+          mesh.matrix.makeRotationFromEuler(mesh.rotation);
+        }
+        Omega.Ship.prototype.dispatchEvent({type: 'loaded_template_mesh', data: mesh});
+        event_cb();
+      }, geometry_prefix);
+
+    //// highlight effects
+      /// each ship instance should set position of mesh
+      var highlight_geometry = new THREE.CylinderGeometry( 0, 40, 80, 8, 2 );
+      var highlight_material = new THREE.MeshBasicMaterial({ color:0x33ff33,
+                                                             shading: THREE.FlatShading } );
+      var highlight_mesh     = new THREE.Mesh(highlight_geometry, highlight_material);
+      highlight_mesh.position.set(Omega.Ship.prototype.highlight_props.x,
+                                  Omega.Ship.prototype.highlight_props.y,
+                                  Omega.Ship.prototype.highlight_props.z);
+      highlight_mesh.rotation.set(Omega.Ship.prototype.highlight_props.rot_x,
+                                  Omega.Ship.prototype.highlight_props.rot_y,
+                                  Omega.Ship.prototype.highlight_props.rot_z);
+      Omega.Ship.gfx[this.type].highlight = highlight_mesh;
+
+    //// lamps
+      var lamps = config.resources.ships[this.type].lamps;
+      Omega.Ship.gfx[this.type].lamps = [];
+      if(lamps){
+        for(var l = 0; l < lamps.length; l++){
+          var lamp  = lamps[l];
+          var slamp = Omega.create_lamp(lamp[0], lamp[1]);
+          slamp.position.set(lamp[2][0], lamp[2][1], lamp[2][2]);
+          Omega.Ship.gfx[this.type].lamps.push(slamp);
+        }
+      }
+  },
+
+  /// invoked cb when resource is loaded, or immediately if resource is already loaded
+  retrieve_resource : function(type, resource, cb){
+    if(!cb && typeof(resource) === "function"){ /// XXX
+       cb = resource; resource = type;
+    }
+
+    switch(resource){
+      case 'template_mesh':
+        if(Omega.Ship.gfx[type] && Omega.Ship.gfx[type].mesh){
+          cb(Omega.Ship.gfx[type].mesh);
+          return;
+        }
+        break;
+      case 'mesh':
+        if(this.mesh){
+          cb(this.mesh);
+          return;
+        }
+        break;
+    }
+
+    var _this = this;
+    this.addEventListener('loaded_' + resource, function(evnt){
+      if(evnt.target == _this) /// event interface defined on prototype, need to distinguish instances
+        cb(evnt.data);
+    });
+  },
+
+  init_gfx : function(config, event_cb){
+    if(this.components.length > 0) return; /// return if already initialized
+    this.load_gfx(config, event_cb);
+
+    var _this = this;
+    Omega.Ship.prototype.retrieve_resource(this.type, 'template_mesh', function(){
+      _this.mesh = Omega.Ship.gfx[_this.type].mesh.clone();
+      if(_this.location)
+        _this.mesh.position.set(_this.location.x,
+                                _this.location.y,
+                                _this.location.z);
+      _this.mesh.omega_entity = _this;
+      _this.dispatchEvent({type: 'loaded_mesh', data: _this.mesh});
+    });
+
+    this.highlight = Omega.Ship.gfx[this.type].highlight.clone();
+    this.highlight.run_effects = Omega.Ship.gfx[this.type].highlight.run_effects; /// XXX
+    if(this.location) this.highlight.position.set(this.location.x, this.location.y, this.location.z);
+
+    this.components = [this.mesh, this.highlight];
+
+    this.lamps = [];
+    for(var l = 0; l < Omega.Ship.gfx[this.type].lamps.length; l++){
+      var lamp = Omega.Ship.gfx[this.type].lamps[l].clone();
+      lamp.run_effects = Omega.Ship.gfx[this.type].lamps[l].run_effects; /// XXX
+      if(this.location)
+        lamp.position.add(new THREE.Vector3(this.location.x,
+                                            this.location.y,
+                                            this.location.z));
+      this.lamps.push(lamp);
+      this.components.push(lamp);
+    }
+  },
+
+  run_effects : function(){
+    for(var l = 0; l < this.lamps.length; l++){
+      var lamp = this.lamps[l];
+      lamp.run_effects();
+    }
+  }
 };
 
 // Return ships owned by the specified user
--- site2/tests/tests/omega/ship.js
+++ site2/tests/tests/omega/ship.js
@@ -1,5 +1,156 @@
 pavlov.specify("Omega.Ship", function(){
 describe("Omega.Ship", function(){
+  describe("#load_gfx", function(){
+    describe("graphics are initialized", function(){
+      var orig;
+
+      before(function(){
+        orig = Omega.Ship.gfx;
+      });
+
+      after(function(){
+        Omega.Ship.gfx = orig;
+      });
+
+      it("does nothing / just returns", function(){
+        Omega.Ship.gfx = {'corvette' : {lamps:null}};
+        new Omega.Ship({type:'corvette'}).load_gfx();
+        assert(Omega.Ship.gfx['corvette'].lamps).isNull();
+      });
+    });
+
+    it("creates mesh for Ship", async(function(){
+      var ship = Omega.Test.Canvas.Entities().ship;
+      ship.retrieve_resource('mesh', function(){
+        assert(Omega.Ship.gfx[ship.type].mesh).isOfType(THREE.Mesh);
+        assert(Omega.Ship.gfx[ship.type].mesh.material).isOfType(THREE.MeshLambertMaterial);
+        assert(Omega.Ship.gfx[ship.type].mesh.geometry).isOfType(THREE.Geometry);
+        start();
+        /// TODO assert material texture & geometry src path values
+      });
+    }));
+
+    it("creates highlight effects for Ship", function(){
+      var ship = Omega.Test.Canvas.Entities().ship;
+      assert(Omega.Ship.gfx[ship.type].highlight).isOfType(THREE.Mesh);
+      assert(Omega.Ship.gfx[ship.type].highlight.material).isOfType(THREE.MeshBasicMaterial);
+      assert(Omega.Ship.gfx[ship.type].highlight.geometry).isOfType(THREE.CylinderGeometry);
+    });
+
+    it("creates lamps for Ship", function(){
+      var ship = Omega.Test.Canvas.Entities().ship;
+      assert(Omega.Ship.gfx[ship.type].lamps.length).equals(Omega.Config.resources.ships[ship.type].lamps.length);
+      for(var l = 0; l < Omega.Ship.gfx[ship.type].lamps.length; l++){
+        var lamp = Omega.Ship.gfx[ship.type].lamps[l];
+        assert(lamp).isOfType(THREE.Mesh);
+        assert(lamp.material).isOfType(THREE.MeshBasicMaterial);
+        assert(lamp.geometry).isOfType(THREE.SphereGeometry);
+      }
+    });
+  });
+
+  describe("#init_gfx", function(){
+    var type = 'corvette';
+    var ship;
+
+    before(function(){
+      /// preiinit using test page
+      Omega.Test.Canvas.Entities();
+
+      ship = new Omega.Ship({type: type,
+        location : new Omega.Location({x: 100, y: -100, z: 200})});
+    });
+
+    after(function(){
+      if(Omega.Ship.gfx){
+        if(Omega.Ship.gfx[type].mesh && Omega.Ship.gfx[type].mesh.clone.restore) Omega.Ship.gfx[type].mesh.clone.restore();
+        if(Omega.Ship.gfx[type].highlight && Omega.Ship.gfx[type].highlight.clone.restore) Omega.Ship.gfx[type].highlight.clone.restore();
+        if(Omega.Ship.gfx[type].lamps)
+          for(var l = 0; l < Omega.Ship.gfx[type].lamps.length; l++)
+            if(Omega.Ship.gfx[type].lamps[l].clone.restore)
+              Omega.Ship.gfx[type].lamps[l].clone.restore();
+      }
+    });
+
+    it("loads ship gfx", function(){
+      var ship   = new Omega.Ship({type: type});
+      var load_gfx  = sinon.spy(ship, 'load_gfx');
+      ship.init_gfx();
+      sinon.assert.called(load_gfx);
+    });
+
+    it("clones Ship mesh", async(function(){
+      var mesh = new THREE.Mesh();
+      /// need to wait till template mesh is loaded b4 wiring up stub
+      Omega.Ship.prototype.
+        retrieve_resource('corvette', 'template_mesh', function(){
+          sinon.stub(Omega.Ship.gfx[type].mesh, 'clone').returns(mesh);
+        });
+
+      ship.init_gfx();
+      ship.retrieve_resource('mesh', function(){
+        assert(ship.mesh).equals(mesh);
+        start();
+      });
+    }));
+
+    it("sets mesh position", async(function(){
+      ship.init_gfx();
+      ship.retrieve_resource('mesh', function(){
+        assert(ship.mesh.position.x).equals(100);
+        assert(ship.mesh.position.y).equals(-100);
+        assert(ship.mesh.position.z).equals(200);
+        start();
+      });
+    }));
+
+    it("sets mesh omega_entity", async(function(){
+      ship.init_gfx();
+      ship.retrieve_resource('mesh', function(){
+        assert(ship.mesh.omega_entity).equals(ship);
+        start();
+      });
+    }));
+
+    it("clones Ship highlight effects", function(){
+      var mesh = new THREE.Mesh();
+      sinon.stub(Omega.Ship.gfx[type].highlight, 'clone').returns(mesh);
+      ship.init_gfx();
+      assert(ship.highlight).equals(mesh);
+    });
+
+    it("clones Ship lamps", function(){
+      var spies = [];
+      for(var l = 0; l < Omega.Ship.gfx[type].lamps.length; l++)
+        spies.push(sinon.spy(Omega.Ship.gfx[type].lamps[l], 'clone'));
+      ship.init_gfx();
+      for(var s = 0; s < spies.length; s++)
+        sinon.assert.called(spies[s]);
+    });
+
+    it("sets scene components to ship mesh, highlight effects, and lamps", function(){
+      ship.init_gfx();
+      var expected = [ship.mesh, ship.highlight].concat(ship.lamps);
+      assert(ship.components).isSameAs(expected);
+    });
+  });
+
+  describe("#run_effects", function(){
+    it("runs lamp effects", function(){
+      var ship = new Omega.Ship({type : 'corvette'});
+      ship.init_gfx();
+
+      var spies = [];
+      for(var l = 0; l < ship.lamps.length; l++)
+        spies.push(sinon.spy(ship.lamps[l], 'run_effects'))
+
+      ship.run_effects();
+
+      for(var s = 0; s < spies.length; s++)
+        sinon.assert.called(spies[s]);
+    });
+  });
+
   describe("#owned_by", function(){
     var node, retrieval_cb, invoke_spy;
 
