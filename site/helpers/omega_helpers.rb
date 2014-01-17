55567adae552d944b1f1aa120db1b165096162f8
Mo Morsi <mo@morsi.org>
2014-01-16 20:53:16 -0500

    UI: add middleman helpers module for omega
    Currently contianing a few simple custom helpers to generate
    an audio tag

---
+++ site/helpers/omega_helpers.rb
@@ -0,0 +1,28 @@
+module OmegaHelpers
+  AUDIO_EXTS  = ['.wav', '.mp3', '.ogg']
+
+  # middleman ships w/ an older vendored version of padrino which
+  # doesn't include the audio_tag helper, so roll a simple one of
+  # our own for now (should be removed when we can leverage padrino's)
+
+  def audio_assets
+    path = "source/#{audio_dir}"
+    Dir["#{path}/**/*"].collect { |audio|
+      if File.directory?(audio) || !AUDIO_EXTS.include?(File.extname(audio))
+        nil
+      else
+        audio.gsub("#{path}/", "")
+      end
+    }.compact
+  end
+
+  def audio_path(src)
+    asset_path(:audio, src).
+      gsub(".audio", "") # XXX quick hack to auto-extension padrino does
+  end
+
+  def audio_tag(url, options={})
+    options.reverse_merge!(:src => audio_path(url))
+    tag(:audio, options)
+  end
+end
