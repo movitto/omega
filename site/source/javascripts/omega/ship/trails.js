99b88da7e6e1467e2ca74ca3a5c4c951a98b3a22
Mo Morsi <mo@morsi.org>
2014-02-19 19:01:38 -0500

    UI: optimize / improve ship trail gfx

--- site/source/javascripts/omega/ship/trails.js
+++ site/source/javascripts/omega/ship/trails.js
@@ -10,19 +10,21 @@ Omega.ShipTrails = function(config, type, event_cb){
 };
 
 Omega.ShipTrails.prototype = {
-  particles_per_second :   3,
-  plane                :   3,
-  lifespan             :   5,
-  particle_speed       :   3,
+  particles_per_second :  30,
+  plane                :   5,
+  lifespan             :   1,
+  particle_speed       :   1,
 
   _particle_velocity : function(){
-    return new THREE.Vector3(0, 0, -this.particle_speed);
+    if(this.__particle_velocity) return this.__particle_velocity;
+    this.__particle_velocity = new THREE.Vector3(0, 0, -this.particle_speed);
+    return this.__particle_velocity;
   },
 
   _particle_group : function(config, event_cb){
     return new ShaderParticleGroup({
       texture:    Omega.load_ship_particles(config, event_cb),
-      maxAge:     this.particle_age,
+      maxAge:     this.lifespan,
       blending:   THREE.AdditiveBlending
     });
   },
@@ -34,12 +36,11 @@ Omega.ShipTrails.prototype = {
       colorEnd           : new THREE.Color(0xFFFFFF),
       sizeStart          :   20,
       sizeEnd            :   20,
-      opacityStart       : 0.75,
-      opacityEnd         : 0.75,
+      opacityStart       :    1,
+      opacityEnd         :    0,
       velocity           : this._particle_velocity(),
       particlesPerSecond : this.particles_per_second,
       alive              :    0,
-      age                : this.lifespan
     });
   },
 
@@ -47,53 +48,57 @@ Omega.ShipTrails.prototype = {
     this.config_trails = config.resources.ships[type].trails;
     if(!this.config_trails) return null;
 
-    var group = this._particle_group(config, event_cb);
+    this.clock     = new THREE.Clock();
+    this.particles = this._particle_group(config, event_cb);
 
     for(var t = 0; t < this.config_trails.length; t++){
-      var emitter = this._particle_emitter();
-      group.addEmitter(emitter);
-    }
+      /// replace config array w/ vector
+      var config_trail = this.config_trails[t];
+      if(config_trail.constructor != THREE.Vector3)
+        this.config_trails[t] =
+          new THREE.Vector3(config_trail[0], config_trail[1], config_trail[2]);
 
-    this.particles = group;
-    this.clock     = new THREE.Clock();
+      /// create new emitter add to group
+      var emitter = this._particle_emitter();
+      this.particles.addEmitter(emitter);
+    }
   },
 
   clone : function(config, type, event_cb){
     return new Omega.ShipTrails(config, type, event_cb);
   },
 
+  _update_emitter : function(e){
+    var entity        = this.omega_entity;
+    var loc           = entity.location;
+    var config_trail  = this.config_trails[e];
+    var emitter       = this.particles.emitters[e];
+
+    /// keep emitter position in sync w/ location
+    emitter.position.set(loc.x, loc.y, loc.z);
+    emitter.position.add(config_trail);
+    Omega.temp_translate(emitter, loc, function(temitter){
+      Omega.rotate_position(temitter, loc.rotation_matrix());
+    });
+
+    /// rotate emitter velocity to match location orientation
+    emitter.velocity = this._particle_velocity();
+    if(entity.mesh)
+      Omega.set_emitter_velocity(emitter, entity.mesh.base_rotation);
+    Omega.set_emitter_velocity(emitter, loc.rotation_matrix());
+    emitter.velocity.multiplyScalar(this.particle_speed);
+  },
+
   update : function(){
     if(!this.config_trails) return;
 
-    var entity = this.omega_entity;
-    var loc    = entity.location;
-
-    for(var t = 0; t < this.config_trails.length; t++){
-      var config_trail  = this.config_trails[t];
-      var config_trailv = new THREE.Vector3(config_trail[0],
-                                            config_trail[1],
-                                            config_trail[2]);
-
-      var emitter       = this.particles.emitters[t];
-      emitter.position.set(loc.x, loc.y, loc.z);
-      emitter.position.add(config_trailv);
-
-      emitter.velocity = this._particle_velocity();
-      if(entity.mesh)
-        Omega.set_emitter_velocity(emitter, entity.mesh.base_rotation);
-      Omega.set_emitter_velocity(emitter, loc.rotation_matrix());
-      emitter.velocity.multiplyScalar(this.particle_speed);
-
-      Omega.temp_translate(emitter, loc, function(temitter){
-        Omega.rotate_position(temitter, loc.rotation_matrix());
-      });
-    }
-
-    /// update 'alive' depending on movement state
     if(this.omega_entity.location.is_stopped())
       this.disable();
-    else
+    else{
       this.enable();
+      for(var t = 0; t < this.config_trails.length; t++)
+        this._update_emitter(t);
+    }
   },
 
   enable : function(){
