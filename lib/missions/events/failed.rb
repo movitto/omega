273e3f1ec2be4f27c5fa86795a434f338d8c3de5
Mo Morsi <mo@morsi.org>
2014-03-06 07:28:14 -0500

    Add missions failure event

---
+++ lib/missions/events/failed.rb
@@ -0,0 +1,25 @@
+# Missions Failed Event definition
+#
+# Copyright (C) 2014 Mohammed Morsi <mo@morsi.org>
+# Licensed under the AGPLv3+ http://www.gnu.org/licenses/agpl.txt
+
+require 'missions/events/mission'
+
+module Missions
+module Events
+
+# Spawned by the local missions subsystem upon mission failure
+class Failed < MissionEvent
+  def type
+    'failed'
+  end
+
+  # Handle event
+  def handle_event
+    mission.failed!
+    update_mission
+  end
+
+end # class Failed
+end # module Events
+end # modjule Missions
