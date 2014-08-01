54b435da5426dc71137032016e8832340e4cbba8
Mo Morsi <mo@morsi.org>
2014-08-01 00:36:06 -0400

    UI: some refactoring of ship module
    No functional changes beyond reorganization

--- site/source/javascripts/omega/ship/gfx/init.js
+++ site/source/javascripts/omega/ship/gfx/init.js
@@ -11,94 +11,110 @@ Omega.ShipGfxInitializer = {
   include_highlight : true,
   include_hp_bar    : true,
 
-  /// Intiialize ship graphics
-  init_gfx : function(config, event_cb){
-    if(this.gfx_initialized()) return;
-    this._gfx_initializing = true;
-    this.load_gfx(config, event_cb);
+  init_components : function(){
     this.components = [];
-
     this.components.push(this.position_tracker());
     this.position_tracker().add(this.location_tracker());
+  },
 
+  init_highlight : function(){
     /// TODO change highlight mesh material if ship doesn't belong to user
     this.highlight = Omega.Ship.gfx[this.type].highlight.clone();
     this.highlight.omega_entity = this;
     if(this.include_highlight)
       this.position_tracker().add(this.highlight.mesh);
+  },
 
+  init_lamps : function(){
     this.lamps = Omega.Ship.gfx[this.type].lamps.clone();
     this.lamps.omega_entity = this;
     this.lamps.init_gfx();
+  },
 
+  init_trails : function(config, event_cb){
     this.trails = Omega.Ship.gfx[this.type].trails.clone(config, this.type, event_cb);
     this.trails.omega_entity = this;
     if(this.trails.particles) this.components.push(this.trails.particles.mesh);
+  },
 
+  init_visited_route : function(){
     this.visited_route = Omega.Ship.gfx[this.type].visited_route.clone();
     this.visited_route.omega_entity = this;
     this.components.push(this.visited_route.line);
+  },
 
+  init_attack_vector : function(config, event_cb){
     /// TODO config option to set weapon(s) originating coordinates on mesh on per-ship-type basis
     this.attack_vector =
       Omega.Ship.gfx[this.type].attack_vector.clone(config, event_cb);
     this.attack_vector.omega_entity = this;
     this.attack_vector.set_position(this.position_tracker().position);
+  },
 
+  init_artillery : function(config, event_cb){
     this.artillery = Omega.Ship.gfx[this.type].artillery.clone(config, event_cb);
     this.artillery.omega_entity = this;
     this.artillery.set_position(this.position_tracker().position);
+  },
 
+  init_mining_vector : function(config, event_cb){
     this.mining_vector =
       Omega.Ship.gfx[this.type].mining_vector.clone(config, event_cb);
     this.mining_vector.omega_entity = this;
     this.components.push(this.mining_vector.particles.mesh);
+  },
 
-    this.trajectory1   = Omega.Ship.gfx[this.type].trajectory1.clone();
+  init_trajectory : function(){
+    this.trajectory1 = Omega.Ship.gfx[this.type].trajectory1.clone();
     this.trajectory1.omega_entity = this;
     this.trajectory1.update();
 
-    this.trajectory2   = Omega.Ship.gfx[this.type].trajectory2.clone();
+    this.trajectory2 = Omega.Ship.gfx[this.type].trajectory2.clone();
     this.trajectory2.omega_entity = this;
     this.trajectory2.update();
+  },
 
+  init_hp_bar : function(config, event_cb){
     this.hp_bar = Omega.Ship.gfx[this.type].hp_bar.clone();
     this.hp_bar.omega_entity = this;
     this.hp_bar.bar.init_gfx(config, event_cb);
     if(this.include_hp_bar)
       for(var c = 0; c < this.hp_bar.bar.components.length; c++)
         this.position_tracker().add(this.hp_bar.bar.components[c]);
+  },
 
+  init_destruction : function(config, event_cb){
     this.destruction = Omega.Ship.gfx[this.type].destruction.clone(config, event_cb);
     this.destruction.omega_entity = this;
     this.destruction.set_position(this.position_tracker().position);
     this.components.push(this.destruction.particles.mesh);
+  },
 
+  init_audio : function(){
     this.destruction_audio = Omega.Ship.gfx[this.type].destruction_audio;
     this.combat_audio = Omega.Ship.gfx[this.type].combat_audio;
     this.movement_audio = Omega.Ship.gfx[this.type].movement_audio;
-
-    this.explosions = Omega.Ship.gfx[this.type].explosions.for_ship(this);
-    this.explosions.omega_entity = this;
-    this.components.push(this.explosions.particles.mesh);
-
-    this.smoke = Omega.Ship.gfx[this.type].smoke.clone();
-    this.smoke.omega_entity = this;
-    this.components.push(this.smoke.particles.mesh);
-
     this.mining_audio = Omega.Ship.gfx[this.type].mining_audio;
     this.docking_audio = Omega.Ship.gfx[this.type].docking_audio;
     this.mining_completed_audio = Omega.Ship.gfx[this.type].mining_completed_audio;
 
-    if(this.attack_component() == this.artillery){
-      this.components.push(this.attack_component().component());
-      this.explosions.interval = this.artillery.interval();
-      this.explosions.auto_trigger = true;
-    }
+  },
 
+  init_explosions : function(){
+    this.explosions = Omega.Ship.gfx[this.type].explosions.for_ship(this);
+    this.explosions.omega_entity = this;
+    this.components.push(this.explosions.particles.mesh);
+  },
+
+  init_smoke : function(){
+    this.smoke = Omega.Ship.gfx[this.type].smoke.clone();
+    this.smoke.omega_entity = this;
+    this.components.push(this.smoke.particles.mesh);
+  },
+
+  init_mesh : function(){
     var _this     = this;
     this.mesh     = Omega.EntityGfxStub.instance();
-    this.missiles = Omega.EntityGfxStub.instance();
 
     Omega.ShipMesh.load(this.type, function(mesh){
       _this.mesh = mesh;
@@ -118,11 +134,49 @@ Omega.ShipGfxInitializer = {
       _this._gfx_initializing = false;
       _this._gfx_initialized  = true;
     });
+  },
+
+  init_missiles : function(){
+    var _this     = this;
+    this.missiles = Omega.EntityGfxStub.instance();
 
     Omega.ShipMissiles.load(this.type, function(missiles){
       _this.missiles = missiles;
       _this.missiles.omega_entity = _this;
     });
+  },
+
+  wire_up_artillery : function(){
+    this.components.push(this.attack_component().component());
+    this.explosions.interval = this.artillery.interval();
+    this.explosions.auto_trigger = true;
+  },
+
+  /// Intiialize ship graphics
+  init_gfx : function(config, event_cb){
+    if(this.gfx_initialized()) return;
+    this._gfx_initializing = true;
+    this.load_gfx(config, event_cb);
+
+    this.init_components();
+    this.init_highlight();
+    this.init_lamps();
+    this.init_trails(config, event_cb);
+    this.init_visited_route();
+    this.init_attack_vector(config, event_cb);
+    this.init_artillery(config, event_cb);
+    this.init_mining_vector(config, event_cb);
+    this.init_trajectory();
+    this.init_hp_bar(config, event_cb);
+    this.init_destruction();
+    this.init_explosions();
+    this.init_smoke();
+
+    if(this.attack_component() == this.artillery)
+      this.wire_up_artillery();
+
+    this.init_mesh();
+    this.init_missiles();
 
     this.last_moved = new Date();
     this.update_gfx();
