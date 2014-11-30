4605c21830ed14b57fb2280400a950841624fa0b
Mo Morsi <mo@morsi.org>
2014-11-30 08:15:32 -0500

    Cleanup omega-movement-plot
    Simply reorg and style fixes / improvements

--- bin/util/omega-movement-plot.rb
+++ bin/util/omega-movement-plot.rb
@@ -14,15 +14,25 @@ require 'optparse'
 require 'omega'
 require 'rjr/nodes/tcp'
 
-config           = OpenStruct.new
-config.ids       = []
-config.labels    = []
-config.url       = 'jsonrpc://localhost:8181'
-config.gnuplot   = '/usr/bin/gnuplot'
-config.delay     = 5
+###
+# Parse CLI
 
-config.quit      = false
-config.locations = {}
+def config
+  @config ||= OpenStruct.new(:ids       => [],
+                             :labels    => [],
+                             :url       => 'jsonrpc://localhost:8181',
+                             :gnuplot   => '/usr/bin/gnuplot',
+                             :delay     => 5,
+                             :quit      => false,
+                             :locations => {})
+end
+
+def verify_params!
+  return if File.executable?(config.gnuplot)
+
+  puts "#{config.gnuplot} must be an executable file"
+  exit 1
+end
 
 optparse = OptionParser.new do |opts|
   opts.on('-h', '--help', 'Display this help screen') do
@@ -54,111 +64,291 @@ optparse = OptionParser.new do |opts|
 end
 
 optparse.parse!
+verify_params!
 
-unless File.executable?(config.gnuplot)
-  puts "#{config.gnuplot} must be an executable file"
-  exit 1
+###
+# Retrieve IDs
+
+def node
+  @node ||= RJR::Nodes::TCP.new :node_id => 'omega-movement-plotter'
 end
 
-config.node = RJR::Nodes::TCP.new :node_id => 'omega-movement-plotter'
-
-if config.ids.size == 0
-  locations = config.node.invoke(config.url, 'motel::get_locations')
-  config.ids = locations.collect { |loc| loc.id }
+def get_ids
+  node.invoke(config.url, 'motel::get_locations')
+      .collect { |loc| loc.id }
 end
 
-if config.ids.size == 0
+def verify_ids!
+  return unless config.ids.empty?
+
   puts "Error no id's found / specified"
   exit 1
 end
 
-def refresh_locations(config)
-  config.ids.each do |id|
-    location = config.node.invoke(config.url, 'motel::get_location', 'with_id', id)
+config.ids = get_ids if config.ids.empty?
+verify_ids!
 
-    config.first_location = config.locations.empty?
-    config.locations[id] ||= {:coordinates_file => Tempfile.new("coordinates-#{id}"),
-                              :orientation_file => Tempfile.new("orientation-#{id}")}
+###
+# Refresh locations
 
-    coordinates_output = "#{location.x} #{location.y} #{location.z}\n"
-    orientation_title  = "#{location.orx.round_to(2)},#{location.ory.round_to(2)},#{location.orz.round_to(2)}"
-    orientation_output = "#{location.orx} #{location.ory} #{location.orz} #{orientation_title}\n"
-    config.locations[id][:coordinates_file].write coordinates_output
-    config.locations[id][:orientation_file].write orientation_output
-    config.locations[id][:coordinates_file].flush
-    config.locations[id][:orientation_file].flush
-
-    if config.first_location
-      config.max = Array.new(location.coordinates)
-      config.min = Array.new(config.max)
-
-    else
-      config.max[0] = location.x if location.x > config.max[0]
-      config.max[1] = location.y if location.y > config.max[1]
-      config.max[2] = location.z if location.z > config.max[2]
-
-      config.min[0] = location.x if location.x < config.min[0]
-      config.min[1] = location.y if location.y < config.min[1]
-      config.min[2] = location.z if location.z < config.min[2]
-    end
-  end
-
-  # if min == max, create range around it
-  config.min[0], config.max[0] = config.min[0] - config.min[0] / 5,
-                                 config.min[0] + config.min[0] / 5 if config.min[0] == config.max[0]
-  config.min[1], config.max[1] = config.min[1] - config.min[1] / 5,
-                                 config.min[1] + config.min[1] / 5 if config.min[1] == config.max[1]
-  config.min[2], config.max[2] = config.min[2] - config.min[2] / 5,
-                                 config.min[2] + config.min[2] / 5 if config.min[2] == config.max[2]
+def get_loc(id)
+  node.invoke(config.url, 'motel::get_location', 'with_id', id)
 end
 
-def plot_locations(config)
-  return if config.first_location # skip first location as axis range won't be valid
-  config.io.puts "set term x11"
+def loc_files(id)
+  {:coordinates_file => Tempfile.new("coordinates-#{id}"),
+   :orientation_file => Tempfile.new("orientation-#{id}")}
+end
 
-  coord_plots  = []
-  orient_plots = []
+def init_loc(id)
+  config.locations[id] ||= loc_files(id)
+end
+
+def coords_file(id)
+  config.locations[id][:coordinates_file]
+end
+
+def orientation_file(id)
+  config.locations[id][:orientation_file]
+end
+
+def coords_output(loc)
+  "#{loc.x} #{loc.y} #{loc.z}\n"
+end
+
+def orient_title(loc)
+  "#{loc.orx.round_to(2)},#{loc.ory.round_to(2)},#{loc.orz.round_to(2)}"
+end
+
+def orientation_output(loc)
+  "#{loc.orx} #{loc.ory} #{loc.orz} #{orient_title(loc)}\n"
+end
+
+def write_coords(loc)
+  f = coords_file(loc.id)
+  f.write coords_output(loc)
+  f.flush
+end
+
+def write_orientation(loc)
+  f = orientation_file(loc.id)
+  f.write orientation_output(loc)
+  f.flush
+end
+
+def write_loc(loc)
+  write_coords(loc)
+  write_orientation(loc)
+end
+
+def init_extrema(loc)
+  config.max = Array.new(loc.coordinates)
+  config.min = Array.new(config.max)
+end
+
+def update_extrema(loc)
+  config.max[0] = loc.x if loc.x > config.max[0]
+  config.max[1] = loc.y if loc.y > config.max[1]
+  config.max[2] = loc.z if loc.z > config.max[2]
+
+  config.min[0] = loc.x if loc.x < config.min[0]
+  config.min[1] = loc.y if loc.y < config.min[1]
+  config.min[2] = loc.z if loc.z < config.min[2]
+end
+
+def create_range
+  xrange = config.min[0] == config.max[0]
+  yrange = config.min[1] == config.max[1]
+  zrange = config.min[2] == config.max[2]
+
+  scale  = 5
+  xmin   = config.min[0] - config.min[0] / scale
+  xmax   = config.min[0] + config.min[0] / scale
+  ymin   = config.min[1] - config.min[1] / scale
+  ymax   = config.min[1] + config.min[1] / scale
+  zmin   = config.min[2] - config.min[2] / scale
+  zmax   = config.min[2] + config.min[2] / scale
+
+  config.min[0], config.max[0] = xmin, xmax if xrange
+  config.min[1], config.max[1] = ymin, ymax if yrange
+  config.min[2], config.max[2] = zmin, zmax if zrange
+end
+
+def refresh_locations
   config.ids.each do |id|
-    title = id[0...15]
-    coord_plots  << "'#{config.locations[id][:coordinates_file].path}' title '#{title}'"
-    orient_plots << "'#{config.locations[id][:orientation_file].path}' title '#{title}', '' using 1:2:3:4 with labels notitle"
+    loc   = get_loc(id)
+    first = config.locations.empty?
+    init_loc(id)
+    write_loc(loc)
+
+    first ? init_extrema(loc) : update_extrema(loc)
+    config.first_location = first
   end
 
-  config.io.puts "set multiplot layout 1,2 title 'Omega Locations'"
+  # if min == max, create range
+  create_range
+end
 
-  config.io.puts "set title 'Coordinates'"
-  config.io.puts "set style data linespoints"
+###
+# Plot Locations
 
-  config.io.puts "set label 1 'center' at 0,0,0"
+def plot_title(id)
+  id[0...15]
+end
+
+def coord_plot_str(id)
+  "'#{coords_file(id).path}' title '#{plot_title(id)}'"
+end
+
+def orient_plot_str(id)
+  "'#{orientation_file(id).path}' title '#{plot_title(id)}', '' using 1:2:3:4 with labels notitle"
+end
+
+def coord_plots
+  config.ids.collect { |id| coord_plot_str(id) }
+end
+
+def orient_plots
+  config.ids.collect { |id| orient_plot_str(id) }
+end
+
+def pipe(str)
+  config.io.puts str
+end
+
+def multiplot_header
+  "set multiplot layout 1,2 title 'Omega Locations'"
+end
+
+def coords_title
+  "set title 'Coordinates'"
+end
+
+def coords_style
+  "set style data linespoints"
+end
+
+def center_label
+  "set label 1 'center' at 0,0,0"
+end
+
+def format_label(label, num)
+  text, x, y, z = *label
+  "set label #{num+1} '#{text}' at #{x},#{y},#{z}"
+end
+
+def set_coords_xrange
+  "set xrange [#{config.min[0]}:#{config.max[0]}]"
+end
+
+def set_coords_yrange
+  "set yrange [#{config.min[1]}:#{config.max[1]}]"
+end
+
+def set_coords_zrange
+  "set zrange [#{config.min[2]}:#{config.max[2]}]"
+end
+
+def coords_plot
+  "splot #{coord_plots.join(',')}"
+end
+
+def plot_coords
+  pipe coords_title
+  pipe coords_style
+  pipe center_label
+
   config.labels.each_index do |i|
-    label, x, y, z = *config.labels[i]
-    config.io.puts "set label #{i+1} '#{label}' at #{x},#{y},#{z}"
+    pipe format_label(config.labels[i], i)
   end
 
-  config.io.puts "set xrange [#{config.min[0]}:#{config.max[0]}]"
-  config.io.puts "set yrange [#{config.min[1]}:#{config.max[1]}]"
-  config.io.puts "set zrange [#{config.min[2]}:#{config.max[2]}]"
-  config.io.puts "splot #{coord_plots.join(',')}"
-
-  config.io.puts "set title 'Orientation'"
-  config.io.puts "set style data points"
-
-  1.upto(config.labels.size) { |i| config.io.puts "unset label #{i}" }
-
-  config.io.puts "set xrange [-1:1]"
-  config.io.puts "set yrange [-1:1]"
-  config.io.puts "set zrange [-1:1]"
-  config.io.puts "splot #{orient_plots.join(',')}"
-
-  config.io.puts "unset multiplot"
+  pipe set_coords_xrange
+  pipe set_coords_yrange
+  pipe set_coords_zrange
+  pipe coords_plot
 end
 
-IO.popen([config.gnuplot, '-noraise'], 'w') do |io|
-  config.io = io
+def orientation_title
+  "set title 'Orientation'"
+end
 
+def orientation_style
+  "set style data points"
+end
+
+def unset_label(num)
+  "unset label #{num}"
+end
+
+def set_orientation_xrange
+  "set xrange [-1:1]"
+end
+
+def set_orientation_yrange
+  "set yrange [-1:1]"
+end
+
+def set_orientation_zrange
+  "set zrange [-1:1]"
+end
+
+def orientations_plot
+  config.io.puts "splot #{orient_plots.join(',')}"
+end
+
+def multiplot_footer
+  "unset multiplot"
+end
+
+def plot_orientation
+  pipe orientation_title
+  pipe orientation_style
+
+  1.upto(config.labels.size) { |i| pipe unset_label(i) }
+
+  pipe set_orientation_xrange
+  pipe set_orientation_yrange
+  pipe set_orientation_zrange
+  pipe orientations_plot
+end
+
+def plot_locations
+  return if config.first_location # skip first location as axis range won't be valid
+  pipe "set term x11"
+  pipe multiplot_header
+  plot_coords
+  plot_orientation
+  pipe multiplot_footer
+end
+
+###
+# Launch GNUPlot
+
+def trap_int
+  trap("INT") do
+    puts "Exiting"
+    exit 0
+  end
+end
+
+def main
+  refresh_locations
+  plot_locations
+end
+
+def run_loop
   until config.quit
-    refresh_locations config
-    plot_locations    config
+    main
     sleep config.delay
   end
 end
+
+def open_pipe
+  IO.popen([config.gnuplot, '-noraise'], 'w') do |io|
+    config.io = io
+    run_loop
+  end
+end
+
+trap_int
+open_pipe
